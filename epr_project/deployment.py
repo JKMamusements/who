ec59577f542da38a79ca8a40c1f8e8f8993c4244
JKMamusements <jkmamusements@gmail.com>
2024-04-15 16:35:12 +0530

    locating domain

--- epr_project/deployment.py
+++ epr_project/deployment.py
@@ -2,7 +2,7 @@ import os
 from .settings import *
 from .settings import BASE_DIR
 
-ALLOWED_HOSTS = ["*"]
+ALLOWED_HOSTS = [os.environ['WEBSITE_HOSTNAME']]
 
 CSRF_TRUSTED_ORIGINS = ['https://'+ os.environ['WEBSITE_HOSTNAME']]
 
