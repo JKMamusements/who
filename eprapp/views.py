19d5bae514754baa0a78938764fe835dca9938ab
JKMamusements <jkmamusements@gmail.com>
2024-04-16 13:07:43 +0530

    adding signup

--- eprapp/views.py
+++ eprapp/views.py
@@ -1,6 +1,30 @@
-from django.shortcuts import render
+####################################
+########### django import s#########
+from django.shortcuts import render, redirect
 from django.http import HttpResponse
+####################################
+
+####################################
+######## Authentications ###########
+from django.contrib.auth import login, authenticate
+####################################
+
+####################################
+############ models ################ 
 from .models import HomePageContent
+####################################
+
+
+####################################
+#############  FORMS    ############
+from .forms import SignUpForm
+####################################
+
+
+
+
+
+
 
 def home(request):
     return render(request, 'index.html')
@@ -15,3 +39,16 @@ def index(request):
         home_page_content = None
 
     return render(request, 'index.html', {'home_page_content': home_page_content})
+
+
+def signup(request):
+    if request.method == "POST":
+        form = SignUpForm(request.POST)
+        if form.is_valid():
+            user = form.save()
+            login(request, user)
+            return redirect('home')
+    else:
+        form = SignUpForm()
+    return render(request, 'signup.html', {'form': form})
+
